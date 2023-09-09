408a96891b0f18ddd2a464e10c669811fae9cee8
AKKatung159 <6434496423@student.chula.ac.th>
2023-09-09 19:47:45 +0700

    lab 4 finish

--- homework4/src/App.java
+++ homework4/src/App.java
@@ -5,36 +5,61 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int tempAmount;
-        List<Integer> tempCoinsList;
-        int[] tempCoins;
-        CoinChange[] ArrayCoinChange = new CoinChange[14];
-        CoinChange tempCoinChange;
-        try {
-            for (int i = 1; i < 15; i++) {
-                File fileTest = new File("./testCase/4." + i + ".txt");
-                Scanner inputScanner = new Scanner(fileTest);
-                tempAmount = inputScanner.nextInt();
-                tempCoinsList = new ArrayList<>();
-                while (inputScanner.hasNextInt()) {
-                    tempCoinsList.add(inputScanner.nextInt());
-                }
-                tempCoins = new int[tempCoinsList.size()];
-                for (int j = 0; j < tempCoinsList.size(); j++) {
-                    tempCoins[j] = tempCoinsList.get(j);
-                }
-                tempCoinChange = new CoinChange(tempAmount, tempCoins);
-                ArrayCoinChange[i - 1] = tempCoinChange;
-                inputScanner.close();
+        // int tempAmount;
+        // List<Integer> tempCoinsList;
+        // int[] tempCoins;
+        // CoinChange[] ArrayCoinChange = new CoinChange[14];
+        // CoinChange tempCoinChange;
+        // try {
+        //     for (int i = 1; i < 15; i++) {
+        //         File fileTest = new File("./testCase/4." + i + ".txt");
+        //         Scanner inputScanner = new Scanner(fileTest);
+        //         tempAmount = inputScanner.nextInt();
+        //         tempCoinsList = new ArrayList<>();
+        //         while (inputScanner.hasNextInt()) {
+        //             tempCoinsList.add(inputScanner.nextInt());
+        //         }
+        //         tempCoins = new int[tempCoinsList.size()];
+        //         for (int j = 0; j < tempCoinsList.size(); j++) {
+        //             tempCoins[j] = tempCoinsList.get(j);
+        //         }
+        //         tempCoinChange = new CoinChange(tempAmount, tempCoins);
+        //         ArrayCoinChange[i - 1] = tempCoinChange;
+        //         inputScanner.close();
+        //     }
+        // } catch (Exception e) {
+        //     System.out.println("Done");
+        // }
+        // // System.out.println(ArrayCoinChange[2]);
+        // for (CoinChange coinChange : ArrayCoinChange) {
+        //     System.out.println(coinChange+"\n");
+        // }
+        
+        int amount = 28;
+        int[] coins = {2, 5, 7};
+        int[] table = new int[amount + 1];
+
+        // Initialization
+        table[0] = 1;
+        System.out.println("Initial table:");
+        printTable(table, amount);
+
+        // Update table for each coin
+        for (int j = 0; j < coins.length; j++) {
+            int coin = coins[j];
+            for (int i = coin; i <= amount; i++) {
+                table[i] += table[i - coin];
             }
-        } catch (Exception e) {
-            System.out.println("Done");
+            System.out.println("\nTable after considering coin of value " + coin + ":");
+            printTable(table, amount);
         }
-        for (CoinChange coinChange : ArrayCoinChange) {
-            // coinChange.WaysToMakeChange(coinChange.Amount, coinChange.Coins[coinChange.CoinIndex]);
-            System.out.println(coinChange);
-            System.out.println(coinChange.getBestSol());
-        }
-            // System.out.println(ArrayCoinChange[1]);
     }
-}
+
+    private static void printTable(int[] table, int amount) {
+        for (int i = 0; i <= amount; i++) {
+            System.out.println("Amount " + i + ": " + table[i] + " ways");
+        }
+    }
+    }
+
+//4 11 9 8 0 1 23 22 47 0 459 8177 4399
--- homework4/src/CoinChange.java
+++ homework4/src/CoinChange.java
@@ -1,40 +1,148 @@
 import java.util.ArrayList;
+// import java.util.HashMap;
 import java.util.List;
+// import java.util.Map;
 
 public class CoinChange {
     private int Amount;
     private int[] Coins;
     private List<List<String>> result;
+    private List<Integer> listAmount;
+    private List<Integer> listIndex;
+    // private Map<String, List<List<String>>> memo = new HashMap<>();
 
     public CoinChange(int Amount, int[] Coins) {
         this.Amount = Amount;
         this.Coins = Coins;
         this.result = new ArrayList<>();
+        this.listAmount = new ArrayList<>();
+        this.listIndex = new ArrayList<>();
+        // this.memo = new HashMap<>();
     }
 
-    public String WaysToMakeChangeEntry() {
-        List<Integer> currentCombination = new ArrayList<>();
-        WaysToMakeChange(0, Amount, currentCombination);
+    // private List<List<String>> WaysToMakeChange(int index, int amount,
+    // List<Integer> current) {
+    // String key = index + "," + amount;
+    // if (memo.containsKey(key)) {
+    // return copyList(memo.get(key));
+    // }
+    // List<List<String>> combinations = new ArrayList<>();
+    // if (amount == 0) {
+    // List<String> toAdd = new ArrayList<>();
+    // for (int num : current) {
+    // toAdd.add(Integer.toString(num));
+    // }
+    // combinations.add(toAdd);
+    // } else if (index < Coins.length) {
+    // key = index + "," + amount;
+    // if (memo.containsKey(key)) {
+    // return memo.get(key);
+    // }
+    // for (int cnt = 0; cnt <= amount / Coins[index]; cnt++) {
+    // for (int j = 0; j < cnt; j++) {
+    // current.add(Coins[index]);
+    // }
+    // combinations.addAll(WaysToMakeChange(index + 1, amount - Coins[index] * cnt,
+    // current));
+    // for (int j = 0; j < cnt; j++) {
+    // current.remove(current.size() - 1);
+    // }
+    // }
+    // memo.put(key, copyList(combinations));
+    // }
 
+    // return combinations;
+    // }
+
+    // private List<List<String>> copyList(List<List<String>> original) {
+    // List<List<String>> copy = new ArrayList<>();
+    // for (List<String> list : original) {
+    // copy.add(new ArrayList<>(list));
+    // }
+    // return copy;
+    // }
+
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Ways to make change = ").append(result.size()).append("\n");
+        sb.append("Amount : ").append(Amount).append("\n");
+        sb.append("coins [] : {");
+        for (int i = 0; i < Coins.length; i++) {
+            sb.append(Coins[i]);
+            if (i != Coins.length - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("}\n");
+
+        List<Integer> currentCombination = new ArrayList<>();
+        result = WaysToMakeChange(0, Amount, currentCombination);
+
+        sb.append("Ways to make change : ").append(result.size()).append("\n");
         for (List<String> combination : result) {
             sb.append("{").append(String.join(",", combination)).append("} ");
         }
-        return sb.toString().trim();
+
+        if (result.size() == 0) {
+            sb.append("\nBestSol : Don't have any solution");
+        } else {
+            sb.append("\nBest Solution is : ");
+            List<String> min = null;
+            for (int i = 0; i < result.size(); i++) {
+                if (min == null) {
+                    min = result.get(i);
+                }
+                if (min.size() > result.get(i).size()) {
+                    min = result.get(i);
+                }
+            }
+            sb.append("{").append(String.join(",", min)).append("} ");
+        }
+
+        return sb.toString();
     }
 
-    private void WaysToMakeChange(int index, int amount, List<Integer> current) {
+    // import java.util.ArrayList;
+    // import java.util.List;
+
+    // public class CoinChange {
+    // private int Amount;
+    // private int[] Coins;
+    // private List<List<String>> result;
+
+    // public CoinChange(int Amount, int[] Coins) {
+    // this.Amount = Amount;
+    // this.Coins = Coins;
+    // this.result = new ArrayList<>();
+    // }
+
+    // public String WaysToMakeChangeEntry() {
+    // List<Integer> currentCombination = new ArrayList<>();
+    // WaysToMakeChange(0, Amount, currentCombination);
+
+    // StringBuilder sb = new StringBuilder();
+    // sb.append("Ways to make change = ").append(result.size()).append("\n");
+    // for (List<String> combination : result) {
+    // sb.append("{").append(String.join(",", combination)).append("} ");
+    // }
+    // return sb.toString().trim();
+    // }
+
+    private List<List<String>> WaysToMakeChange(int index, int amount, List<Integer> current) {
+        // for (int i = 0; i < listAmount.size(); i++) {
+        //     if (listAmount.get(i) == amount && listIndex.get(i) == index) {
+        //         return result;
+        //     }
+        // }
         if (amount == 0) {
             List<String> toAdd = new ArrayList<>();
             for (int num : current) {
                 toAdd.add(Integer.toString(num));
             }
             result.add(toAdd);
-            return;
+            return result;
         }
         if (index == Coins.length || amount < 0) {
-            return;
+            return result;
         }
 
         // Choose the current coin
@@ -44,26 +152,31 @@ public class CoinChange {
         // Don't choose the current coin
         current.remove(current.size() - 1);
         WaysToMakeChange(index + 1, amount, current);
+
+        listAmount.add(amount);
+        listIndex.add(index);
+        return result;
     }
 
-    public String toString() {
-        String line = "";
-        line += "Amount = " + Amount;
-        line += "\ncoins [] = {";
-        for (int i = 0; i < Coins.length; i++) {
-            line += Coins[i] + ",";
-        }
-        line += "}";
-        line += "\n" + WaysToMakeChangeEntry();
-        // for (int i = 0; i < 3; i++) {
-        //     // line += memo[i][Amount];
-        // }
-        return line;
-    }
-    public String getBestSol(){
-        if(result.size()==0){
-            return "BestSol : Null";
-        }
-        return "BestSol : "+result.get(result.size()-1);
-    }
+    // public String toString() {
+    // String line = "";
+    // line += "Amount = " + Amount;
+    // line += "\ncoins [] = {";
+    // for (int i = 0; i < Coins.length; i++) {
+    // line += Coins[i] + ",";
+    // }
+    // line += "}";
+    // line += "\n" + WaysToMakeChangeEntry();
+    // // for (int i = 0; i < 3; i++) {
+    // // // line += memo[i][Amount];
+    // // }
+    // return line;
+    // }
+    // public String getBestSol(){
+    // if(result.size()==0){
+    // return "BestSol : Null";
+    // }
+    // return "BestSol : "+result.get(result.size()-1);
+    // }
+    // }
 }
